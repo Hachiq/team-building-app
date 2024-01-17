52b2f64d5356110ad9e5fbda27200d9e30a3aedd
Hachiq <lesiv433@gmail.com>
2024-01-17 11:04:12 +0200

    Add CORS and authentication services

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,9 @@
 using Api.Data;
 using Api.Services.UserService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Api
 {
@@ -22,11 +25,35 @@ namespace Api
 
             builder.Services.AddControllers();
 
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                        .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            builder.Services.AddCors(options => options.AddPolicy(name: "NgOrigins",
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+                    }
+                ));
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
+            app.UseCors("NgOrigins");
+
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 

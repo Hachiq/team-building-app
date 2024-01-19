6e2b2726dff6f888124bd2d490f212dc2a8da142
Hachiq <lesiv433@gmail.com>
2024-01-19 15:52:36 +0200

    Complete login component. Store JWT in local storage

--- Client/src/app/components/login/login.component.ts
+++ Client/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Login } from 'src/app/models/login';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  user: Login = {
+    username: '',
+    password: ''
+  }
 
+  hide = true;
+
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+
+  constructor(private authService: AuthService) { }
+
+  login() {
+    this.authService.login(this.user).subscribe((token: string) => {
+      localStorage.setItem('authToken', token);
+    },
+      (error) => {
+        if(error.status === 400){
+          this.username.setErrors({ nomatch: true });
+          console.log(error.error);
+        }
+        else {
+          console.log("Undefined error. Please, try again later")
+        }
+      }
+    );
+  }
+
+  getUsernameErrorMessage() {
+    return this.username.hasError('nomatch') ? 'User not found or wrong password' : '';
+  }
 }

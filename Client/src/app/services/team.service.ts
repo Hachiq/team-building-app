2036a7bff7549b90ed12befd7b98741bae155261
Hachiq <lesiv433@gmail.com>
2024-01-31 14:37:42 +0200

    Update client service

--- Client/src/app/services/team.service.ts
+++ Client/src/app/services/team.service.ts
@@ -4,6 +4,7 @@ import { Team } from '../models/team';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { TeamRequest } from '../interfaces/teamRequest';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,18 @@ export class TeamService {
   public get(): Observable<Team[]> {
     return this.http.get<Team[]>(
       `${environment.apiUrl}/team/all`
+    )
+  }
+
+  public getById(id: number): Observable<Team> {
+    return this.http.get<Team>(
+      `${environment.apiUrl}/team/${id}`
+    )
+  }
+
+  public getUsersByTeamId(id: number): Observable<User[]> {
+    return this.http.get<User[]>(
+      `${environment.apiUrl}/team/${id}/users`
     )
   }
 

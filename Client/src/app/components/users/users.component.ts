11a97d3141a079e6b1a22a03adb81950440aaf72
Hachiq <lesiv433@gmail.com>
2024-05-07 15:40:28 +0300

    Add filtering for users table

--- Client/src/app/components/users/users.component.ts
+++ Client/src/app/components/users/users.component.ts
@@ -29,4 +29,9 @@ export class UsersComponent {
       } 
     );
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
